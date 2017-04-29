35d8db7079faef734cbb0af214b67a9f34d36c5b
fedor <fedja.balashov@gmail.com>
2017-04-29 17:49:13 +0200

    update

---
+++ AmazeJam/Assets/LGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LGenerator : MonoBehaviour {
+
+public GameObject [] large;
+Transform fpc;
+
+	// Use this for initialization
+	void Start () {
+	fpc = GameObject.Find("FPSController").transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+
+	}
+}
