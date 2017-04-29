67ec13037ba19f3281535b5c322d35f42e91c30b
fedor <fedja.balashov@gmail.com>
2017-04-29 19:16:40 +0200

    Upd2

---
+++ AmazeJam/Assets/Generate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour {
+
+public Transform player;
+Vector3 pos;
+
+GameObject currentObject;
+bool isgenerated;
+public GameObject [] creations;
+
+
+	// Use this for initialization
+	void Start () {
+//	InvokeRepeating("LaunchGen", 0f, f);
+	StartCoroutine (GenerateObj());
+
+	}
+
+	IEnumerator GenerateObj(){
+	while(true){
+	yield return new WaitForSeconds(4f);
+	pos = player.position + Vector3.forward * 100;
+
+	currentObject = Instantiate(creations[Random.Range(0, creations.Length)], pos, Quaternion.identity) as GameObject;
+
+
+	}
+
+
+
+	}
+
+}
--- AmazeJam/Assets/LGenerator.cs
+++ AmazeJam/Assets/LGenerator.cs
@@ -6,15 +6,37 @@ public class LGenerator : MonoBehaviour {
 
 public GameObject [] large;
 Transform fpc;
+int index;
+GameObject currentObject;
+bool isgenerated;
 
 	// Use this for initialization
 	void Start () {
 	fpc = GameObject.Find("FPSController").transform;
-		
+	index = Random.Range(0, large.Length);
+	isgenerated = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	float d = Vector3.Distance(fpc.position, transform.position);
+	if (d  >600f){
+	isgenerated = true;
+
+	}
+	else {
+	isgenerated = false;
+	}
+
+
+	if (isgenerated){
+	currentObject = Instantiate(large[index]) as GameObject;
+	currentObject.transform.parent = gameObject.transform;
+
+	isgenerated = false;
+
+
+	}
 
 
 
