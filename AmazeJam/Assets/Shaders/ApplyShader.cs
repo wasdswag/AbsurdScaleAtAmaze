149c5a57e77e2d114989c39aecdbc3992fa5adfc
Leon <leon.denise@free.fr>
2017-04-29 19:15:23 +0200

    hop

---
+++ AmazeJam/Assets/Shaders/ApplyShader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyShader : MonoBehaviour {
+
+	public Shader shader;
+
+	void Start () {
+		Renderer[] rendererArray = GetComponentsInChildren<Renderer>();
+		for (int i = 0; i < rendererArray.Length; ++i) {
+			Renderer renderer = rendererArray[i];
+			Material[] materials = renderer.materials;
+			Material[] mats = new Material[materials.Length];
+			for (int m = 0; m < materials.Length; ++m) {
+				Material old = materials[m];
+				mats[m] = new Material(shader);
+				mats[m].color = old.color;
+				if (old.mainTexture != null) {
+					mats[m].mainTexture = old.mainTexture;
+					mats[m].SetFloat("_ColorNormal", 0f);
+				} else {
+					mats[m].SetFloat("_ColorNormal", 1f);
+				}
+			}
+			renderer.materials = mats;
+		}
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ AmazeJam/Assets/Shaders/Resources/ShaderPass.shader
@@ -0,0 +1,40 @@
+﻿Shader "Hidden/ShaderPass"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			#include "Utils.cginc"
+			
+			sampler2D _MainTex;
+			sampler2D _CameraTexture;
+
+			fixed4 frag (v2f_img i) : SV_Target
+			{
+				float2 uv = i.uv;
+				float unit = 2.0 / _ScreenParams.xy;
+				float2 offset = float2(0.5,0.5)-i.uv;
+				float angle = rgb2hsv(tex2D(_MainTex, i.uv)).x * PI2;
+				offset.xy += float2(cos(angle),sin(angle));
+				uv += offset * unit;
+				fixed4 buffer = tex2D(_MainTex, uv);
+				fixed4 camera = tex2D(_CameraTexture, i.uv);
+				fixed4 color = lerp(buffer, camera, camera.a);
+				return color;
+			}
+			ENDCG
+		}
+	}
+}
